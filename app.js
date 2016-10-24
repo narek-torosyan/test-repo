79fc492305a09bf184fdf2a6eb1237611201388f
narek-torosyan <narek.torosyan@volo.global>
2016-10-24 19:13:18 +0400

    stage test

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 var fs = require('fs')
-fs.readFile(__filename, {encoding:"utf-8"}, function(err, data){
-    console.log(data);
+var http = require("http")
+
+
+fs.stat(__filename, function (err, stat) {
+    var x = stat.isFile();
+    console.log(stat);
+}
+);
+fs.readFile(__filename, { encoding: "utf-8" }, function (err, data) {
+    console.log()
+    console.log(__filename)
+    if (err) {
+        console.log(err);
+    }
+    
+    else {
+        console.log(data);
+        }
 })
